a370f30721049cddd006b05cd1a7b62d830ee908
Dru-Daniels <dancergirl_23@hotmail.com>
2021-05-25 12:28:32 -0400

    NavBar Created

---
+++ src/AboutPage.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const AboutPage = (props) => {
+
+  return (
+    <div>
+      <h1>Hello from AboutHello from AboutHello from About</h1>
+      <h1>Hello from AboutHello from AboutHello from About</h1>
+      <h1>Hello from AboutHello from AboutHello from About</h1>
+      <h1>Hello from AboutHello from AboutHello from About</h1>
+      <h1>Hello from AboutHello from AboutHello from About</h1>
+      <h1>Hello from AboutHello from AboutHello from About</h1>
+      <h1>Hello from AboutHello from AboutHello from About</h1>
+      <h1>Hello from AboutHello from AboutHello from About</h1>
+      <h1>Hello from AboutHello from AboutHello from About</h1>
+      <h1>Hello from AboutHello from AboutHello from About</h1>
+      <h1>Hello from AboutHello from AboutHello from About</h1>
+      <h1>Hello from AboutHello from AboutHello from About</h1>
+      <h1>Hello from AboutHello from AboutHello from About</h1>
+      <h1>Hello from AboutHello from AboutHello from About</h1>
+      <h1>Hello from AboutHello from AboutHello from About</h1>
+      <h1>Hello from AboutHello from AboutHello from About</h1>
+      <h1>Hello from AboutHello from AboutHello from About</h1>
+    </div>
+  )
+}
+
+export default AboutPage
