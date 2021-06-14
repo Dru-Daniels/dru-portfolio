bdfa0755c98ea4f0da1c46bb9af26eb52ecfb96a
Dru-Daniels <dancergirl_23@hotmail.com>
2021-06-14 13:25:32 -0400

    Refactor variables plust icon anim

--- src/components/Feature.js
+++ src/components/Feature.js
@@ -7,7 +7,6 @@ import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 
 import { gsap } from "gsap/dist/gsap";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
-
 gsap.registerPlugin(ScrollTrigger);
 
 const Feature = (props) => {  
@@ -46,20 +45,39 @@ const Feature = (props) => {
     })
   }, [])
 
-  const contentOpera = <a href="https://youtu.be/GPFTQ6Od1MM?t=2037" target="blank" className="featured-title" >Opera</a>
+  const contentOpera = (
+    <a 
+      href="https://youtu.be/GPFTQ6Od1MM?t=2037" 
+      target="blank" 
+      className="featured-title" 
+    >Opera</a>
+  )
+
+  const contentTech = (
+    <a 
+      href="https://soko-wars.netlify.app/" 
+      target="blank" 
+      className="featured-title" 
+    >Techie</a>
+  )
+  
   const titleOpera = "Hear Dru Sing"
-  const contentTech = <a href="https://soko-wars.netlify.app/" target="blank" className="featured-title" >Techie</a>
   const titleTech = "Play Soko Wars"
+
+  const description =(
+    "Opera singer turned techie, I love creative problem solving--whether it be onstage or online. I enjoy working with those who value mentorship, best practices, keeping up with the latest tech, diversity, and having a positive impact. Feel free to reach out!"
+  )
 
   return (
     <section className="feature" id="about">
+      
       <div className="left">
         <div className="inner transition2">
           <p className="subtitle">About Dru</p>
-          <span >
-            <Tooltip title={titleOpera} content={contentOpera} /> <SwapHorizIcon/> <Tooltip title={titleTech} content={contentTech}/>
-          </span>
-          <p className="featured-desc">Opera singer turned techie, I love creative problem solving--whether it be onstage or online. I enjoy working with those who value mentorship, best practices, keeping up with the latest tech, diversity, and having a positive impact. Feel free to reach out!</p>
+          <Tooltip title={titleOpera} content={contentOpera} /> 
+          <SwapHorizIcon className="feature-icon" /> 
+          <Tooltip title={titleTech} content={contentTech}/>
+          <p className="featured-desc">{description}</p>
           <a 
             className="primary-btn" 
             target="_blank" 
@@ -70,7 +88,9 @@ const Feature = (props) => {
           </a>
         </div>
       </div>
-      <img className="right transition2 dru-img" src="/DruPic.png" alt="Dru Headshot"></img>
+
+      <img className="right transition2 dru-img" src="/DruPic.png" alt="Dru Headshot"/>
+
     </section>
   )
 }
